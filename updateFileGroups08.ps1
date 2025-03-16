995621d611f93beab764562cad6c0c2af5e62960
konge <iancblauer@gmail.com>
2025-03-16 10:48:31 -0500

    Delete updateFileGroups08.ps1

--- updateFileGroups08.ps1
+++
@@ -1,3 +0,0 @@
-$members = "help_decrypt.*|decrypt_instructions.*|restore_files.*|*.encrypted|*.cryptolocker|help_your_files.*|*.vvv|*.crypt|_locky_recover_instructions.*|_h_e_l_p_recover_instructions*|de_crypt_readme*"
-
-filescrn filegroup modify /filegroup:"Cryptolocker Files" /members:$members
